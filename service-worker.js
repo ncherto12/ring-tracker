15280c615f7a35f5c09e384547502edd601d1378
Noam Chertoff <noam.chertoff@mail.huji.ac.il>
2026-08-17 01:59:07 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // ==================================================
 
 const CACHE_NAME =
-    "ring-tracker-v4";
+    "ring-tracker-v5";
 
 const APP_FILES = [
     "./",
@@ -229,67 +229,3 @@ async function loadRequest(
         return Response.error();
     }
 }
-
-// ==================================================
-// Notification click
-// ==================================================
-
-self.addEventListener(
-    "notificationclick",
-    function (event) {
-
-        event.notification.close();
-
-
-        const targetUrl =
-            event.notification.data &&
-            event.notification.data.url
-                ?
-                event.notification.data.url
-                :
-                "./";
-
-
-        event.waitUntil(
-            clients
-                .matchAll(
-                    {
-                        type:
-                            "window",
-
-                        includeUncontrolled:
-                            true
-                    }
-                )
-                .then(
-                    function (clientList) {
-
-                        for (
-                            const client
-                            of clientList
-                        ) {
-
-                            if (
-                                "focus"
-                                in
-                                client
-                            ) {
-
-                                client.navigate(
-                                    targetUrl
-                                );
-
-
-                                return client.focus();
-                            }
-                        }
-
-
-                        return clients.openWindow(
-                            targetUrl
-                        );
-                    }
-                )
-        );
-    }
-);
